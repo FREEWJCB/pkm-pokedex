34a52a47448e075ce39c904bb849131a8030a39a
Walter Caro <freelance.wjcb@gmail.com>
2025-06-28 22:41:50 -0500

    Fix ESLint override for tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export */
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -6,21 +5,19 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Compatibilidad con configuración tradicional (como .eslintrc)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// Exportamos la configuración
-export default [
-  // Extensiones estándar para Next.js y TypeScript
+const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-  // Overrides para permitir 'any' en archivos de test y setup
   {
-    files: ["**/*.test.ts", "**/setup.ts"],
+    files: ["src/test/setup.ts", "**/*.test.ts"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
+
+export default eslintConfig;
