5f32f14edbf16449cc3845888bfd39ac1f19b5be
Walter Caro <freelance.wjcb@gmail.com>
2025-06-28 22:28:46 -0500

    refactor: Simplificar la configuración de ESLint y mejorar la legibilidad del código

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -5,12 +6,21 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Compatibilidad con configuración tradicional (como .eslintrc)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+// Exportamos la configuración
+export default [
+  // Extensiones estándar para Next.js y TypeScript
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
-export default eslintConfig;
+  // Overrides para permitir 'any' en archivos de test y setup
+  {
+    files: ["**/*.test.ts", "**/setup.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
